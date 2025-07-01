833094bd4a780a24296db48634745b4d14290ea6
İbrahim Gurses <igrses9@gmail.com>
2025-07-01 11:36:48 +0300

    Update MyAssistant.tsx

--- components/MyAssistant.tsx
+++ components/MyAssistant.tsx
@@ -12,8 +12,8 @@ export function MyAssistant() {
           message: "Ask me questions about Peaka Sample Data",
           suggestions: [
             {
-              prompt: "List me top 10 opportunities from crm.",
-              text: "List me top 10 opportunities from crm.",
+              prompt: "List me top 10 opportunities from hubspot.",
+              text: "List me top 10 opportunities from hubspot.",
             },
             {
               prompt: "List me top 10 actors from database.",
